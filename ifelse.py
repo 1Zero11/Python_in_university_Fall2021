dababb68fbccda3492d0a9ba635ec22af50b763f
1Zero11 <mroallll54@gmail.com>
2021-09-07 17:52:34 +0300

    Доделал задания на условия

--- ifelse.py
+++ ifelse.py
@@ -1,3 +1,4 @@
+import itertools
 
 #Ход короля
 def g():
@@ -6,7 +7,7 @@ def g():
     x2 = int(input())
     y2 = int(input())
 
-    if abs(x2-x1)<=1 and abs(y2-y1)<=1:
+    if abs(x2-x1)>=1 and abs(y2-y1)>=1:
         print('YES')
     else:
         print('NO')
@@ -33,10 +34,53 @@ def Chocolate():
     m = int(input())
     k = int(input())
 
-    if n == k or m ==k:
+    if (n*m-k)%m == 0 or (m*n-k)%n == 0:
         print('YES')
     else:
         print('NO')
 
+#Узник замка
+def Castle_prisoner():
+    A = int(input())
+    B = int(input())
+    C = int(input())
+    D = int(input())
+    E = int(input())
+
+    if (A<=E and B<=D) or (A<=E and C<=D) or (B<=E and A<=D) or (B<=E and C<=D) or (C<=E and A<=D) or (C<=E and B<=D):
+        print('YES')
+    else:
+        print('NO')
+
+# Коробки
+def Cardboards():
+    A1 = int(input())
+    B1 = int(input())
+    C1 = int(input())
+    A2 = int(input())
+    B2 = int(input())
+    C2 = int(input())
+
+    dimensions1 = [A1, B1, C1]
+    dimensions2 = [A2, B2, C2]
+
+    possible = list(itertools.permutations([A1, B1, C1]))
+    # print(possible)
+
+    for dimensions in possible:
+        if(dimensions[0]<=A2 and dimensions[1]<=B2 and dimensions[2]<=C2 or
+                dimensions[0]>=A2 and dimensions[1]>=A2 and dimensions[2]>=A2):
+            print('YES')
+            return
+    print('NO')
+
+#Котлеты
+def Cotlets():
+    k = int(input())
+    m = int(input())
+    n = int(input())
+
+    print((n//k + 1 - (0**(n%m)))*2*m)
+
 if __name__ == "__main__":
-    Chocolate()
+    Cotlets()
